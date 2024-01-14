402e5531911856f26de7be9f2cb45cac5b1c80b6
Victor <121985984+Victormasa@users.noreply.github.com>
2024-01-14 22:11:50 +0000

    a

---
+++ numbers/triangles.py
@@ -0,0 +1,14 @@
+def equilateral(sides):
+    pass
+
+
+def isosceles(sides):
+    pass
+
+
+def scalene(sides):
+    pass
+
+sides = [2,2,2]
+if sides[0] == sides[1] == sides[2]:
+    print (True)    
